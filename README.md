a8a3c921b780bd330f9d673b8e7b3ed3b188a0f9
Ouzw <ouzhiwei@jointcharging.com>
2024-11-19 23:45:14 +0800

    chore: uploading tmux profiles of terminal.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # termConf
 the tmux config for terminal
+
+# Install
+```bash
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -aivf tmux_conf.template ~/.tmux.conf
+cp -aivf tmux_conf_local.template ~/.tmux.conf.local
